bfc724a0b0f7ec002a8f6ca05a327762f16278ea
rafe-forward <rforward@smu.edu>
2025-04-21 21:51:59 -0500

    fixed comment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const app = express();
+
+//port for application
 const port = 3000;
 
 const receiptsRoutes = require("./routes/receipts");
 app.use(express.json());
 
+// use receipts route
 app.use("/receipts",receiptsRoutes);
 
 // Starts the server
